a87c611dce9cc6582fc731c0ae3d3d2c0ac93ba8
Zac <3253345336@qq.com>
2023-03-09 16:36:52 +0800

    update match thrift

--- thrift/match.thrift
+++ thrift/match.thrift
@@ -1,13 +1,11 @@
 namespace cpp match_service
 
-#### 定义一个user结构体
 struct User {
     1: i32 id,
     2: string name,
     3: i32 score
 }
 
-#### 定义一个函数
 service Mathch{
     i32 add_user(1:User user,2:string info),
 
